59ca11209df3acc045be92eb47f31ba7bef1de71
ranjithduvasi <duvasiranjithkumar@gmail.com>
2023-01-11 14:46:19 +0530

    first commit

---
+++ src/main/java/com/mvnspring/SpringMvnApplication.java
@@ -0,0 +1,13 @@
+package com.mvnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvnspring/SpringMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
